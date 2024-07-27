865108eb33bbf4b5a4e460c66679ee493392492a
Colin Lyons <lyonscf@pm.me>
2024-07-27 22:18:54 +0100

    Adding a network selector.

--- cmd/ghia/crawler/crawl.go
+++ cmd/ghia/crawler/crawl.go
@@ -1,10 +1,13 @@
 package crawler
 
 import (
+	"github.com/davecgh/go-spew/spew"
 	"github.com/ghia-xch/ghia/pkg"
+	"github.com/ghia-xch/ghia/pkg/protocol/network"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
 )
 
 var (
@@ -18,6 +21,18 @@ var crawlCommand = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		l.Println("-- ghia (" + viper.GetString("network") + ") - " + pkg.SemVer + " - PoST Freedom. --")
+
+		var err error
+		var net *network.Network
+
+		if net, err = network.Select(viper.GetString("network")); err != nil {
+
+			l.Fatal(err)
+
+			os.Exit(1)
+		}
+
+		spew.Dump(net)
 
 		l.Debugln("DEBUG!")
 
--- pkg/protocol/network/types.go
+++ pkg/protocol/network/types.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"errors"
 	"github.com/ghia-xch/ghia/pkg/protocol/primitive"
 )
 
@@ -16,4 +17,18 @@ var DefaultNetwork *Network = Mainnet
 
 func NewNetwork(str string) *Network {
 	return &Network{primitive.NewString(str)}
+}
+
+func Select(str string) (*Network, error) {
+
+	switch str {
+	case "mainnet":
+		return Mainnet, nil
+	case "testnet":
+		return Testnet, nil
+	case "simnet":
+		return Simnet, nil
+	default:
+		return nil, errors.New("'" + str + "' is not a valid network.")
+	}
 }
