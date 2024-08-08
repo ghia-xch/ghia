b9cd6de0d0fe46a9a8508a3d7bf6b925cf39fb22
Colin Lyons <lyonscf@pm.me>
2024-08-08 20:57:09 +0100

    refactoring to protocol.

--- pkg/node/protocol/introducer/spec.go
+++ pkg/node/protocol/introducer/spec.go
@@ -1,33 +1,33 @@
 package introducer
 
 import (
-	protocol2 "github.com/ghia-xch/ghia/pkg/node/protocol"
+	"github.com/ghia-xch/ghia/pkg/node/protocol"
 	"github.com/ghia-xch/ghia/pkg/peer"
 )
 
 type RequestPeersIntroducer byte
 
-func (r *RequestPeersIntroducer) Encode(enc *protocol2.MessageEncoder) (em protocol2.EncodedMessage, err error) {
+func (r *RequestPeersIntroducer) Encode(enc *protocol.MessageEncoder) (em protocol.EncodedMessage, err error) {
 
-	enc.Reset(protocol2.RequestPeersIntroducer, nil)
+	enc.Reset(protocol.RequestPeersIntroducer, nil)
 
 	return enc.Encode()
 }
 
-func (r *RequestPeersIntroducer) Decode(dec *protocol2.MessageDecoder, em protocol2.EncodedMessage) (err error) {
+func (r *RequestPeersIntroducer) Decode(dec *protocol.MessageDecoder, em protocol.EncodedMessage) (err error) {
 	return nil
 }
 
 type RespondPeersIntroducer []*peer.TimestamptedPeerInfo
 
-func (r *RespondPeersIntroducer) Encode(enc *protocol2.MessageEncoder) (em protocol2.EncodedMessage, err error) {
+func (r *RespondPeersIntroducer) Encode(enc *protocol.MessageEncoder) (em protocol.EncodedMessage, err error) {
 
-	enc.Reset(protocol2.RespondPeersIntroducer, nil)
+	enc.Reset(protocol.RespondPeersIntroducer, nil)
 
 	return enc.Encode(r)
 }
 
-func (r *RespondPeersIntroducer) Decode(dec *protocol2.MessageDecoder, em protocol2.EncodedMessage) (err error) {
+func (r *RespondPeersIntroducer) Decode(dec *protocol.MessageDecoder, em protocol.EncodedMessage) (err error) {
 
 	if err = dec.Reset(em); err != nil {
 		return
