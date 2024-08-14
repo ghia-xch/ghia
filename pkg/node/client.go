0f9d68c67ea2a86aaae7d85bab033a08489bc740
Colin Lyons <lyonscf@pm.me>
2024-08-14 22:10:17 +0100

    adding drainers to queuing.

--- pkg/node/client.go
+++ pkg/node/client.go
@@ -202,36 +202,6 @@ func (c *Client) handleInboundMessage(dec *protocol.MessageDecoder, em protocol.
 	return nil
 }
 
-func (c *Client) drainInboundQueue(dec *protocol.MessageDecoder) (err error) {
-
-	var inboundLen int
-
-	inboundLen = len(c.inbound)
-
-	for i := 0; i < inboundLen; i++ {
-		if err = c.handleInboundMessage(dec, <-c.inbound); err != nil {
-			return
-		}
-	}
-
-	return nil
-}
-
-func (c *Client) drainOutboundQueue() (err error) {
-
-	var outboundLen int
-
-	outboundLen = len(c.outbound)
-
-	for i := 0; i < outboundLen; i++ {
-		if err = c.conn.WriteMessage(websocket.BinaryMessage, <-c.outbound); err != nil {
-			l.Errorln("error writing to connection: %v", err)
-		}
-	}
-
-	return nil
-}
-
 func (c *Client) Handle(handlers ...protocol.MessageHandler) {
 	for _, handler := range handlers {
 		c.handlers[handler.Type] = handler.Callback
--- pkg/node/queuing.go
+++ pkg/node/queuing.go
@@ -1,1 +1,36 @@
 package node
+
+import (
+	"github.com/ghia-xch/ghia/pkg/node/protocol"
+	"github.com/gorilla/websocket"
+)
+
+func (c *Client) drainInboundQueue(dec *protocol.MessageDecoder) (err error) {
+
+	var inboundLen int
+
+	inboundLen = len(c.inbound)
+
+	for i := 0; i < inboundLen; i++ {
+		if err = c.handleInboundMessage(dec, <-c.inbound); err != nil {
+			return
+		}
+	}
+
+	return nil
+}
+
+func (c *Client) drainOutboundQueue() (err error) {
+
+	var outboundLen int
+
+	outboundLen = len(c.outbound)
+
+	for i := 0; i < outboundLen; i++ {
+		if err = c.conn.WriteMessage(websocket.BinaryMessage, <-c.outbound); err != nil {
+			l.Errorln("error writing to connection: %v", err)
+		}
+	}
+
+	return nil
+}
