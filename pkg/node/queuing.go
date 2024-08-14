ae9cb2635ce622e249c9e10127a8bcea771c3e74
Colin Lyons <lyonscf@pm.me>
2024-08-14 22:12:43 +0100

    adding inbound and outbound queuing to queuing.

--- pkg/node/queuing.go
+++ pkg/node/queuing.go
@@ -3,7 +3,75 @@ package node
 import (
 	"github.com/ghia-xch/ghia/pkg/node/protocol"
 	"github.com/gorilla/websocket"
+	"time"
 )
+
+func (c *Client) inboundQueuing() {
+
+	l.Infoln("starting inbound queuing")
+
+	var err error
+	var em []byte
+
+	for {
+
+		if _, em, err = c.conn.ReadMessage(); err != nil {
+
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+
+				l.Errorln("error reading from connection: %v", err)
+
+				c.isClosing <- true
+
+				return
+			}
+
+			continue
+		}
+
+		c.inbound <- em
+	}
+}
+
+const PingInterval = 60 * time.Second
+
+func (c *Client) outboundQueuing() {
+
+	l.Infoln("starting outbound queuing")
+
+	var err error
+	var em protocol.EncodedMessage
+	var ok bool
+
+	ticker := time.NewTicker(PingInterval)
+
+	defer ticker.Stop()
+
+	for {
+		select {
+		case em, ok = <-c.outbound:
+
+			if !ok {
+				continue
+			}
+
+			if err = c.conn.WriteMessage(websocket.BinaryMessage, em); err != nil {
+				l.Errorln("error writing to connection: %v", err)
+			}
+
+		case <-ticker.C:
+
+			l.Infoln("sending ping")
+
+			if err = c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				l.Println("write:", err)
+				return
+			}
+
+			ticker.Reset(10 * time.Second)
+		}
+	}
+}
 
 func (c *Client) drainInboundQueue(dec *protocol.MessageDecoder) (err error) {
 
