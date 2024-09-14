94eda5e0ddcf3b0a4a06acff9a6b781557821404
Colin Lyons <lyonscf@pm.me>
2024-09-14 20:51:52 +0100

    removing python code from peering.

--- pkg/node/protocol/full_node/peering.go
+++ pkg/node/protocol/full_node/peering.go
@@ -6,21 +6,9 @@ import (
 	"github.com/ghia-xch/ghia/pkg/peer"
 )
 
-//@streamable
-//@dataclass(frozen=True)
-//class RequestPeers(Streamable):
-//"""
-//Return full list of peers
-//"""
-
 type RequestPeers struct{}
 
 func (rp *RequestPeers) Type() message.Type { return protocol.RequestPeers }
-
-//@streamable
-//@dataclass(frozen=True)
-//class RespondPeers(Streamable):
-//peer_list: List[TimestampedPeerInfo]
 
 type RespondPeers struct {
 	Peers []peer.TimestamptedPeerInfo
