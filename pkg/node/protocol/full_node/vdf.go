e524c64a2379364a1281f439568fa9e0585c767b
Colin Lyons <lyonscf@pm.me>
2024-09-15 21:22:52 +0100

    adding new compact VDF

--- pkg/node/protocol/full_node/vdf.go
+++ pkg/node/protocol/full_node/vdf.go
@@ -1,15 +1,19 @@
 package full_node
 
-//
-//@streamable
-//@dataclass(frozen=True)
-//class NewCompactVDF(Streamable):
-//height: uint32
-//header_hash: bytes32
-//field_vdf: uint8
-//vdf_info: VDFInfo
-//
-//
+import (
+	"github.com/ghia-xch/ghia/pkg/node/protocol"
+	"github.com/ghia-xch/ghia/pkg/node/protocol/message"
+)
+
+type NewCompactVDF struct {
+	Height     uint32
+	HeaderHash protocol.Hash
+	FieldVDF   uint8
+	VDFInfo    VDFInfo
+}
+
+func (c *NewCompactVDF) Type() message.Type { return protocol.NewCompactVDF }
+
 //@streamable
 //@dataclass(frozen=True)
 //class RequestCompactVDF(Streamable):
