70eee4f128c200d70b391ca251596af7914ba5ea
Colin Lyons <lyonscf@pm.me>
2024-09-15 21:08:01 +0100

    removing old encoder/decoder from capability set.

--- pkg/node/capability/set.go
+++ pkg/node/capability/set.go
@@ -1,9 +1,5 @@
 package capability
 
-import (
-	"github.com/ghia-xch/ghia/pkg/node/protocol/message"
-)
-
 var DefaultSet = map[Capability]string{
 	Base:         "1",
 	BlockHeaders: "1",
@@ -23,46 +19,4 @@ func (s Set) IsEnabled(capability Capability) bool {
 	}
 
 	return true
-}
-
-// Encode expects a List[Tuple[uint16,string]]
-func (s Set) Encode(enc *message.MessageEncoder) (em message.EncodedMessage, err error) {
-
-	if em, err = enc.Encode(uint32(len(s))); err != nil {
-		return
-	}
-
-	for key, value := range s {
-		if em, err = enc.Encode(uint16(key), value); err != nil {
-			return
-		}
-	}
-
-	return enc.Bytes(), nil
-}
-
-func (s Set) Decode(dec *message.MessageDecoder) (err error) {
-
-	var setLen uint32
-	var capIndex uint16
-	var capIsSet string
-
-	if setLen, err = dec.ParseUint32(); err != nil {
-		return
-	}
-
-	for i := 0; i < int(setLen); i++ {
-
-		if capIndex, err = dec.ParseUint16(); err != nil {
-			return
-		}
-
-		if capIsSet, err = dec.ParseString(); err != nil {
-			return
-		}
-
-		s[Capability(capIndex)] = capIsSet
-	}
-
-	return nil
 }
