6c74017c5f1149b900d6768a7108981cbe3c39e2
Colin Lyons <lyonscf@pm.me>
2024-09-15 21:24:05 +0100

    adding request compact VDF.

--- pkg/node/protocol/full_node/vdf.go
+++ pkg/node/protocol/full_node/vdf.go
@@ -14,15 +14,15 @@ type NewCompactVDF struct {
 
 func (c *NewCompactVDF) Type() message.Type { return protocol.NewCompactVDF }
 
-//@streamable
-//@dataclass(frozen=True)
-//class RequestCompactVDF(Streamable):
-//height: uint32
-//header_hash: bytes32
-//field_vdf: uint8
-//vdf_info: VDFInfo
-//
-//
+type RequestCompactVDF struct {
+	Height     uint32
+	HeaderHash protocol.Hash
+	FieldVDF   uint8
+	VDFInfo    VDFInfo
+}
+
+func (c *RequestCompactVDF) Type() message.Type { return protocol.RequestCompactVDF }
+
 //@streamable
 //@dataclass(frozen=True)
 //class RespondCompactVDF(Streamable):
