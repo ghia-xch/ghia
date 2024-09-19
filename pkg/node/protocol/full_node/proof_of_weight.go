cd8ea60ba39ebccb92d1d75fa4d9d2e588d5ecac
Colin Lyons <lyonscf@pm.me>
2024-09-19 12:42:18 +0100

    adding proof of space

--- pkg/node/protocol/full_node/proof_of_weight.go
+++ pkg/node/protocol/full_node/proof_of_weight.go
@@ -4,6 +4,8 @@ import (
 	"github.com/ghia-xch/ghia/pkg/node/protocol"
 	"github.com/ghia-xch/ghia/pkg/node/protocol/message"
 	"github.com/ghia-xch/ghia/pkg/node/protocol/vdf"
+	bls12381 "github.com/kilic/bls12-381"
+	"lukechampine.com/uint128"
 )
 
 type RequestProofOfWeight struct {
@@ -24,23 +26,55 @@ func (r *RespondProofOfWeight) Type() message.Type { return protocol.RespondProo
 ///
 ///
 
+//#[streamable]
+//pub struct ProofOfSpace {
+//challenge: Bytes32,
+//pool_public_key: Option<G1Element>,
+//pool_contract_puzzle_hash: Option<Bytes32>,
+//plot_public_key: G1Element,
+//size: u8,
+//proof: Bytes,
+//}
+
 type HeaderBlock struct {
 }
 
+type ProofOfSpace struct {
+	Challenge              protocol.Hash
+	PoolPublicKey          *bls12381.PointG1 `cenc:"optional"`
+	PoolContractPuzzleHash *protocol.Hash    `cenc:"optional"`
+	PlotPublicKey          bls12381.PointG1
+	Size                   uint8
+	Proof                  []byte
+}
+
 type SubSlotData struct {
+	ProofOfSpace      *ProofOfSpace    `cenc:"optional"`
+	CCSignagePoint    *vdf.VDFProof    `cenc:"optional"`
+	CCInfusionPoint   *vdf.VDFProof    `cenc:"optional"`
+	ICCInfusionPoint  *vdf.VDFProof    `cenc:"optional"`
+	CCSPVDFInfo       *vdf.VDFInfo     `cenc:"optional"`
+	SignagePointIndex *uint8           `cenc:"optional"`
+	CCSlotEnd         *vdf.VDFProof    `cenc:"optional"`
+	ICCSlotEnd        *vdf.VDFProof    `cenc:"optional"`
+	CCSlotEndInfo     *vdf.VDFInfo     `cenc:"optional"`
+	ICCSlotEndInfo    *vdf.VDFInfo     `cenc:"optional"`
+	CCIPVDFInfo       *vdf.VDFInfo     `cenc:"optional"`
+	ICCIPVDFInfo      *vdf.VDFInfo     `cenc:"optional"`
+	TotalIterations   *uint128.Uint128 `cenc:"optional"`
 }
 
 type SubEpochChallengeSegment struct {
 	SubEpochN     uint32
 	SubSlots      []SubSlotData
-	RcSlotEndInfo *vdf.VDFInfo
+	RcSlotEndInfo *vdf.VDFInfo `cenc:"optional"`
 }
 
 type SubEpochData struct {
 	RewardChainHash      protocol.Hash
 	NumBlocksOverflow    uint8
-	NewSubSlotIterations *uint64
-	NewDifficulty        *uint64
+	NewSubSlotIterations *uint64 `cenc:"optional"`
+	NewDifficulty        *uint64 `cenc:"optional"`
 }
 
 type WeightProof struct {
